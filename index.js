272c7a42143a44c44ca31102e54a55f510b213d1
YongHoonJJo <starctak@gmail.com>
2019-01-08 16:27:45 +0900

    "<li" 로 시작하는 문자열 "@@@" 으로 변경 테스트

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ function testDoc(req, res) {
             results[i] = $(elem).html().replace(/(\r\n\t|\n|\r\t)/gm, " "); // 개행 제거하기 (플랫폼 상관 없음)
             results[i] = results[i].replace(/\t+/g,"");             // \t 제거하기
             results[i] = results[i].replace(/  +/g, ' ').trim();    // 여러 개의 공백을 하나의 공백으로 변경
+            results[i] = results[i].replace(/(<li([^>]+)>)/ig,"@@@");    // 태그 제거하기
             //console.log(results[i] + '\n');
             console.log(results[i]);
         });
