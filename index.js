410b178c5bbfcdf9d41efc8d2f8a35577d3746fb
YongHoonJJo <starctak@gmail.com>
2019-01-08 15:37:46 +0900

    testDoc 함수 분리 및 실행결과 txt 파일로 추출.

--- index.js
+++ index.js
@@ -18,33 +18,10 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());                         // json data 를
 app.use(bodyParser.urlencoded({extended:true}));    // urlencoded data 를 담는다.
 
+app.get("/docs", testDoc);
 app.post("/docs", parseDocs);
 
-app.get("/docs", (req, res) => {
-    const url = "https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/concepts.html";
-
-    request({url, encoding: null}, (err, response, body) => {
-        let htmlDoc = iconv.convert(body).toString();
-        //console.log(htmlDoc); // Get HTML Source
-
-        let results = [];
-
-        const $ = cheerio.load(htmlDoc);
-
-        let docMain = $('div#main-col-body').children().each( (i, elem) => {
-            results[i] = $(elem).html().replace(/(\r\n\t|\n|\r\t)/gm, " "); // 개행 제거하기 (플랫폼 상관 없음)
-        });
-        for(let i=0; i<results.length; i++) {
-            results[i] = results[i].replace(/(<([^>]+)>)/ig,"");    // 태그 제거하기
-            results[i] = results[i].replace(/\t+/g,"");             // \t 제거하기
-            results[i] = results[i].replace(/  +/g, ' ').trim();    // 여러 개의 공백을 하나의 공백으로 변경
-            console.log(results[i] + '\n');
-        }
-        res.send("success");
-    });
-});
-
-app.listen(3000, () => {
+app.listen(3005, () => {
     console.log('listening on port 3000');
 });
 
@@ -74,6 +51,32 @@ function parseDocs(req, res) {
             results[i] = results[i].replace(/  +/g, ' ').trim();    // 여러 개의 공백을 하나의 공백으로 변경
             console.log(results[i] + '\n');
         }
-        res.render("result");
+        res.send("Success");
+        //res.render("result");
+    });
+}
+
+function testDoc(req, res) {
+    const url = "https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/concepts.html";
+
+    request({url, encoding: null}, (err, response, body) => {
+        let htmlDoc = iconv.convert(body).toString();
+        //console.log(htmlDoc); // Get HTML Source
+
+        let results = [];
+
+        const $ = cheerio.load(htmlDoc);
+
+        let docMain = $('div#main-col-body').children().each( (i, elem) => {
+            results[i] = $(elem).html().replace(/(\r\n\t|\n|\r\t)/gm, " "); // 개행 제거하기 (플랫폼 상관 없음)
+            results[i] = results[i].replace(/\t+/g,"");             // \t 제거하기
+            results[i] = results[i].replace(/  +/g, ' ').trim();    // 여러 개의 공백을 하나의 공백으로 변경
+            //console.log(results[i] + '\n');
+            console.log(results[i]);
+        });
+        for(let i=0; i<results.length; i++) {
+            results[i] = results[i].replace(/(<([^>]+)>)/ig,"");    // 태그 제거하기
+        }
+        res.send("success");
     });
 }
